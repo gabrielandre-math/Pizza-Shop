c3a19eedf75aa71d5c14bc17fe6ca6e9c5ec6f4a
Gabriel André <gabrieldevpyc@gmail.com>
2025-02-27 03:35:19 -0300

    Conexão com a API - 0%

--- src/pages/app/orders/order-table-row.tsx
+++ src/pages/app/orders/order-table-row.tsx
@@ -1,41 +1,52 @@
-import { ArrowRight } from "lucide-react";
-import { TableCell, TableCell, TableRow } from "../../../components/ui/table";
-import { Button } from "../../../components/ui/button";
+import { ArrowRight, Search, X } from 'lucide-react'
+import { TableCell, TableRow } from '../../../components/ui/table'
+import { Button } from '../../../components/ui/button'
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+} from '../../../components/ui/dialog'
+import { OrderDetails } from './order-details'
 
-export interface OrderTableRowProps{ 
-
-}
-export function OrderTableRow(props: OrderTableRowProps) {
-    return (
-        <TableRow key={i}>
-                                <TableCell>
-                                    <Button variant="outline" size="xs">
-                                        <Search className="h-3 w-3" />
-                                        <span className="sr-only">Detalhes do pedido</span>
-                                    </Button>
-                                </TableCell>  
-                                <TableCell className="font-mono text-xs font-medium">91872398127391</TableCell>  
-                                <TableCell className="text-muted-foreground">há 15 minutos</TableCell>  
-                                <TableCell>
-                                    <div className="flex items-center gap-2">
-                                        <span className="h-2 w-2 rounded-full bg-slate-400" />    
-                                        <span className="font-medium text-muted-foreground">Pendente</span> 
-                                    </div>    
-                                </TableCell>  
-                                <TableCell className="font-medium">Gabriel André</TableCell>  
-                                <TableCell className="font-medium">R$ 149,99</TableCell>  
-                                <TableCell>
-                                    <Button variant="outline">
-                                        <ArrowRight className="mr-2 h-3 w-3"/>
-                                        Aprovar    
-                                    </Button>
-                                </TableCell>
-                                <TableCell>
-                                    <Button variant="ghost" size="xs">
-                                        <X className="h-3 w-3 mr-2"/>
-                                        Cancelar
-                                    </Button>    
-                                </TableCell>  
-                            </TableRow>    
-    )
+//export interface OrderTableRowProps {}
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button variant="outline" size="xs">
+              <Search className="h-3 w-3" />
+              <span className="sr-only">Detalhes do pedido</span>
+            </Button>
+          </DialogTrigger>
+          <OrderDetails />
+        </Dialog>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">
+        91872398127391
+      </TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Gabriel André</TableCell>
+      <TableCell className="font-medium">R$ 149,99</TableCell>
+      <TableCell>
+        <Button variant="outline">
+          <ArrowRight className="mr-2 h-3 w-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="h-3 w-3 mr-2" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
 }
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,26 +1,28 @@
 import { createBrowserRouter } from 'react-router-dom'
-import { Dashboard } from './pages/app/dashboard'
 import { SignUp } from './pages/auth/sign-up'
 import { SignIn } from './pages/auth/sign-in'
 import { AppLayout } from './pages/_layouts/app'
 import { AuthLayout } from './pages/_layouts/auth'
 import { Orders } from './pages/app/orders/orders'
+import { Dashboard } from './pages/app/dashboard/dashboard'
+import { NotFound } from './pages/404'
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <AppLayout />,
+    errorElement: <NotFound />,
     children: [
       { index: true, path: '/', element: <Dashboard /> },
       { index: true, path: '/orders', element: <Orders /> },
     ],
   },
   {
-    path: "/",
+    path: '/',
     element: <AuthLayout />,
     children: [
-      { path: "/sign-in", element: <SignIn /> },
-      { path: "/sign-up", element: <SignUp /> },
+      { path: '/sign-in', element: <SignIn /> },
+      { path: '/sign-up', element: <SignUp /> },
     ],
   },
 ])
